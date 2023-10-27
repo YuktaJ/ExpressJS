98d4712673ff70f4a83ff4e7eca95c9e6b747399
Yukta Jotiba  Patil <yuktapatil1820@gmail.com>
2023-10-27 23:33:11 +0530

    task 2

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
-
 const app = express();
+const BodyParser = require("body-parser");
 
-app.use("/", (req, res, next) => {
-    console.log("Hi 1");
-    next();
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+    res.send('<form action = "/nextpage" method="POST"><input type ="text" name = "Title"><input type="number" name ="size" required><button type="submit"> Submit</button></form>')
 })
-
-app.use("/", (req, res, next) => {
-    res.send("<h1>Hello Second Time</h1>");
+app.use("/nextpage", (req, res, next) => {
+    console.log(req.body.Title, req.body.size);
+    res.redirect("/add-product");
 })
-
 app.listen(3000);
